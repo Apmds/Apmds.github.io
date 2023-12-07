ed9007c004d2136cc328481d5147c74a7b024929
Apmds <ap1mrfj@gmail.com>
2023-12-07 14:16:09 +0000

    Suporte para login com userData.json

--- login.js
+++ login.js
@@ -1,23 +1,40 @@
+/*
+$.getJSON("https://github.com/Apmds/Apmds.github.io/blob/main/userData.json")
+.done(function( data ) {
+   console.log(data)
+})
+*/
+
+const githubRepoUrl = 'https://raw.githubusercontent.com/Apmds/Apmds.github.io/main/userData.json';
+var user_logins = {}
+
+
+fetch(githubRepoUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Failed to fetch JSON file: ${response.status} ${response.statusText}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Process the JSON data here
+    Object.entries(data).forEach(user => {
+        user_logins[user[0]] = user[1]
+    })
+    
+    console.log(user_logins);
+  })
+  .catch(error => {
+    console.error('Error fetching JSON file:', error);
+  });
+
 function check_login() {
     error_msg = document.getElementById("error_msg")
     email = document.getElementById("email").value.trim()
     password = document.getElementById("password").value
-    logins = {
-        "samuel.ramos.sgfr@gmail.com": {
-            "password": "1234"
-        },
-        "goncalosimoes511@gmail.com": {
-            "password": "4321"
-        },
-        "goncaloliveirasilva@gmail.com": {
-            "password": "gsil"
-        },
-        "ap1mrfj@gmail.com": {
-            "password": "A1Sd"
-        }
-    }
-    if (Object.keys(logins).includes(email)) {
-        var right_password = logins[email]["password"]
+    
+    if (Object.keys(user_logins).includes(email)) {
+        var right_password = user_logins[email]["password"]
         console.log(password)
         console.log(right_password)
         if (password == right_password) {
